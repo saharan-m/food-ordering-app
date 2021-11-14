b8ea3dccacedb1b3169f3c647caa08af190c3e32
saharan-m <mayanksaharan18@gmail.com>
2021-11-14 18:27:04 +0530

    after adding ability to add or remove items

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,12 +1,34 @@
+import React,{useContext,useEffect,useState} from 'react';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css'
 const HeaderCartButton = props =>{
-    return <button className={classes.button}>
+    const [bump,setBump] = useState(false)
+    const cartCtx = useContext(CartContext);
+    const {items} = cartCtx;
+    const numberOfCartItems = cartCtx.items.reduce((current,item)=>{
+        return current + item.amount;
+    },0)
+    const btnClasses = `${classes.button} ${bump?classes.bump:''}`
+
+    useEffect(()=>{
+        if(items.length === 0) return;
+        setBump(true)
+        
+        const timer = setTimeout(()=>{
+            setBump(false)
+        },300)
+        return ()=>{
+            clearTimeout(timer)
+        }
+    },[items])
+
+    return <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon/>
         </span>
         <span>Your Cart</span>
-        <span className={classes.badge}>3</span>
+        <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
 }
 
